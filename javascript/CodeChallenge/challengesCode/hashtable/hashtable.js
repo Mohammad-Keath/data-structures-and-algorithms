623a1a0b7bf11f77164ddf13a3bf6f17e28b3dd2
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-08-23 17:25:22 +0000

    first try

---
+++ javascript/CodeChallenge/challengesCode/hashtable/hashtable.js
@@ -0,0 +1,57 @@
+"use strict";
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    append(value) {
+        const node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
+    }
+    print() {
+        let values = [];
+        let current = this.head;
+        while (current) {
+            values.push(current.value);
+            current = current.next;
+        }
+        return values;
+    }
+}
+class Hashmap {
+    constructor(size) {
+        this.size = size;
+        this.map = new Array(size);
+    }
+    makeHash(key) {
+        const asciiCodeSum = key.split("").reduce((acc, char) => {
+            return acc + char.charCodeAt();
+        }, 0);
+        const multiPrime = asciiCodeSum * 599;
+        const theIndex = multiPrime % this.size;
+        return theIndex;
+    }
+    add(key, value) {
+        const hash = this.makeHash(key);
+        if(!this.map[hash]){
+            this.map[hash] = new LinkedList();
+        }
+
+        this.map[hash].append({ [key]: value })
+    }
+}
+
+export default Hashmap

2a393e1d19b9f09686563bf2bb919af426e08abf
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-09-14 14:51:21 +0000

    depth

---
+++ javascript/CodeChallenge/challengesCode/graph/graph-depth-first/graph-depth-first.md
@@ -0,0 +1,4 @@
+# graph-depth-first
+[Code](../graph//graph.js)
+![graph-depth-first](./graph-depth-first.png)
+![graph-depth-first test](./graph-depth-first%20test.png)
--- javascript/CodeChallenge/challengesCode/graph/graph/graph.js
+++ javascript/CodeChallenge/challengesCode/graph/graph/graph.js
@@ -94,6 +94,32 @@ class Graph{
         }
         return cost     
     }
+    DepthFirst(root) {
+        const visited = [];
+        const queue = [];
+        const result = [];
+    
+        queue.push(root);
+        visited.push(root);
+        result.push(root.value)
+        const recursion = (node)=>{
+            let neighbors = this.getNeighbors(node)
+            neighbors.map((neighbor)=>{
+                if (!visited.includes(neighbor.vertex)) {
+                    queue.push(neighbor.vertex);
+                    visited.push(neighbor.vertex);
+                    if(!result.includes(neighbor.vertex.value)){
+                        result.push(neighbor.vertex.value);
+                }
+                }
+                recursion(queue.shift())
+            }) 
+        }
+        recursion(queue.shift());
+        
+    
+        return result;
+      }
 }
 module.exports=Graph
 
